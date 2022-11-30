b1292c985786ef3a25b7ce0890ad603d79d01992
Mariya <m_relaxeena@mail.ru>
2022-11-30 17:37:21 +0400

    task25

---
+++ Homework4/task25/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число А");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int Exponentiation(int numA, int numB)
+{
+    int result = 1;
+    for (int i = 1; i <= numB; i++)
+    {
+        result = result * numA;
+    }
+    return result;
+}
+int exponentiation = Exponentiation(numA, numB);
+Console.WriteLine($"{numA} в степени {numB} = {exponentiation}");
