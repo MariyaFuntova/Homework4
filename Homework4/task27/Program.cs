d0e33956089cf57cf7020a8ab22bbfd47b0c2ea5
Mariya <m_relaxeena@mail.ru>
2022-11-30 17:58:19 +0400

    task27

---
+++ Homework4/task27/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+int sumInNumber(int numberA)
+{
+    int sum = 0;
+    while (numberA > 0)
+    {
+        int mod =numberA % 10;
+        sum = sum + mod;
+        int div = numberA / 10;
+        numberA = div;
+    }
+    return sum;
+}
+int sum = sumInNumber(numberA);
+Console.WriteLine($"Сумма цифр в числе {numberA} = {sum}");
